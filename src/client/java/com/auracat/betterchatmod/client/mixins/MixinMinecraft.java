3da8e67d7401dfc09f217cbaada8afe00474d6cd
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-09-25 19:10:09 +0200

    1.1.0 release
    + empty startGameMixin inject in MixinMinecraft

--- src/client/java/com/auracat/betterchatmod/client/mixins/MixinMinecraft.java
+++ src/client/java/com/auracat/betterchatmod/client/mixins/MixinMinecraft.java
@@ -3,7 +3,14 @@ package com.auracat.betterchatmod.client.mixins;
 import com.auracat.betterchatmod.client.IWithPastSentMessages;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public abstract class MixinMinecraft implements Runnable, IWithPastSentMessages {
+    @Inject(method = "startGame", at = @At(value = "RETURN"))
+    public void startGameMixin(CallbackInfo ci) {
+
+    }
 }
