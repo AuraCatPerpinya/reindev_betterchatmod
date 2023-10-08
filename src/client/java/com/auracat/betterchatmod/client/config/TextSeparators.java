5854fcec733c667c8099b2808f114e6356cdb78d
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-10-09 01:30:05 +0200

    add more text separators

--- src/client/java/com/auracat/betterchatmod/client/config/TextSeparators.java
+++ src/client/java/com/auracat/betterchatmod/client/config/TextSeparators.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class TextSeparators {
     public List<Character> include = Arrays.asList(' ');
-    public List<Character> exclude = Arrays.asList(',', '.');
+    public List<Character> exclude = Arrays.asList(',', '.', ';', '?', '!', '-', '_', ':', '&');
 
     public List<Character> getMergedList() {
         List<Character> list = new ArrayList<>();
